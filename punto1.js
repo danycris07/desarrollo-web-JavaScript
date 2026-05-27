9c3afd068c11ecc27491b7d7ea8fdbf105037db5
Cristaldo Jonathan <cristaldojonathandaniel2021@gmail.com>
2026-05-27 14:20:07 -0300

    Wip: se inicio trabajo de JS

---
+++ punto1.js
@@ -0,0 +1,7 @@
+const nombre = "Jonathan Daniel Cristaldo";
+let edad = 18;
+let carrera = "Tecnicatura Superior en Desarrollo de Software Multiplataforma";
+let cursa = true;
+
+console.log (`Hola, soy ${nombre} y tengo ${edad} estoy cursando la carrera: ${carrera} Y actualmente la estoy cursando: ${cursa}`)
+
