23c20bec17bcb33b77f05fc33ce98af70b15df2c
Cristaldo Jonathan <cristaldojonathandaniel2021@gmail.com>
2026-05-28 09:59:30 -0300

    Feat: se agrego y realizo la actividad 6

---
+++ punto6.js
@@ -0,0 +1,9 @@
+for (let numero = 1; numero <= 20; numero++) {
+
+    if (numero % 2 === 0) {
+        console.log(numero + " es par");
+    } else {
+        console.log(numero + " es impar");
+    }
+
+}
